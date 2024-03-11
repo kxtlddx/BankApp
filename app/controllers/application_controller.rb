11ed3aba842bcb48cdf9e840b465654a5102a121
kxtldd_x <91431716+kxtlddx@users.noreply.github.com>
2024-03-11 11:41:41 +0300

    1я практическая
    Добавила главную страницу. Так же, добавлено:
    
    1. Хэширования пароля и хранения его в БД для аутентификации при входе
    2. Регистрацию пользователя
    3. Запоминания пользователя посредством cookie
    4. Валидацию
    5. Скорректирован контроллер таким образом, чтобы для регистрации и обновления аккаунта пользователя devise принимал first_name и last_name
    6. Скорректирован view для регистрации и обновления аккаунта, так чтобы они отражали необходимые поля
    7. Обновлен HomeController так, чтобы залогиненного пользователя редиректило на страницу редактирования аккаунта, а незалогиненного - на страницу логина
    8. Проверена корректность:
        1. Валидаций данных при регистрации
        2. Регистрации пользователя
        3. Обновления аккаунта пользователя
        4. Log in / Log out из аккаунта
        5. Попытка зайти на страницу редактирования аккаунта незалогиненным пользователем
        6. Удаление аккаунта

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,38 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
+
+  def after_sign_up_path_for(resource)
+    '/users/edit'
+  end
+
+  def after_sign_in_path_for(resource)
+    '/users/edit'
+  end
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
+
+  def update_resource(resource, params)
+    resource.update_with_password(params)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation,
+      :current_password
+    )
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  validates :email, length: { maximum: 150 }, format: { with: /\A[a-zA-Z0-9. _-]+@[a-zA-Z0-9. -]+\.[a-zA-Z]{2,4}\z/ }
+  validates :first_name, length: { minimum:5, maximum: 100 }
+  validates :last_name, length: { minimum:5, maximum: 100 }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
